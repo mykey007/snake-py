a84eee0e86dc8a37ad8c91da27e59a1169d92d30
Michael Mallowe <mmallowe@gmail.com>
2020-04-13 20:10:03 -0700

    setup board

---
+++ snake.py
@@ -0,0 +1,17 @@
+# Simple Snake Game in Python 3
+# By ToykyoEdTech
+
+import turtle
+
+# Setup the screen
+
+wn = turtle.Screen()
+wn.title("Snake Game")
+wn.bgcolor("green")
+wn.setup(width="600", height="0")
+# Prevent animation
+wn.tracer(0)
+# keep window open
+wn.mainloop()
+
+
